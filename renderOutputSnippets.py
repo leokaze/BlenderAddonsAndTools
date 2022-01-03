bea6a3451e9c16d9067fb795b4c292365cf149cf
leokaze <caceleo@gmail.com>
2022-01-02 20:56:36 -0400

    actualizado a blender 3, operador separato a funcion para crear el path y previsualiar el path

--- renderOutputSnippets.py
+++ renderOutputSnippets.py
@@ -1,149 +1,175 @@
+from ntpath import join
+from bpy.props import BoolProperty, StringProperty, PointerProperty
+import os
+import bpy
 bl_info = {
     "name": "Render Output Snippets",
     "author": "leokaze",
-    "version": (0, 1),
-    "blender": (2, 80, 0),
-    "location": "View3D > Object > ",
+    "version": (0, 2),
+    "blender": (3, 0, 0),
+    "location": "Output Properties > Render Output Snippets",
     "description": "Set output render path to varios snippets predefined",
     "warning": "",
     "wiki_url": "",
     "tracker_url": "",
     "category": "Render"}
 
-import bpy
-import os
-from bpy.props import BoolProperty, StringProperty, PointerProperty
+def GetPath(context):
+    props = context.scene.render_output_snippets_props
 
+    fileName = (os.path.basename(bpy.data.filepath))
+    dotIndex = fileName.rindex(".")
+    fileName = fileName[:dotIndex]
+
+    sceneName = bpy.context.scene.name
+    layerName = bpy.context.view_layer.name
+    cameraName = "NoCamera" if bpy.context.scene.camera == None else bpy.context.scene.camera.name
+
+
+    renderPath = props.folderPath
+    if(renderPath[-1] == "\\"):
+        renderPath = renderPath[:-1]
+    renderName = ""
+
+    if(props.fileName):
+        renderPath += "\\" + fileName
+    if(props.sceneName):
+        renderPath += "\\" + sceneName
+    if(props.layerName):
+        renderPath += "\\" + layerName
+    if(props.cameraName):
+        renderPath += "\\" + cameraName
+    if(props.customName):
+        renderPath += "\\" + props.customFolder
+
+    if(props.ffileName):
+        renderName = fileName
+    if(props.fsceneName):
+        renderName += sceneName if len(renderName) == 0 else props.fseparator + sceneName
+    if(props.flayerName):
+        renderName += layerName if len(renderName) == 0 else props.fseparator + layerName
+    if(props.fcameraName):
+        renderName += cameraName if len(renderName) == 0 else props.fseparator + cameraName
+    if(props.fcustomName):
+        renderName += props.fcustomFileName if len(renderName) == 0 else props.fseparator + props.fcustomFileName
+    if(props.fseparatorEnd):
+        renderName += props.fseparator
+
+    if(len(renderPath) == 0):
+        renderPath = "//"
+    if(len(renderName) == 0):
+        renderName = "MyRender"
+
+    return renderPath + "\\" + renderName
 
 class RenderOutputSnippetsOperator(bpy.types.Operator):
     """ToolTip of RenderOutputSnippetsOperator"""
-    bl_idname = "addongen.render_output_snippets_operator"
+    bl_idname = "leo_tools.render_output_snippets_operator"
     bl_label = "Render Output Snippets Operator"
     bl_options = {'REGISTER'}
 
-    
-    #@classmethod
-    #def poll(cls, context):
-    #    return context.object is not None
-
     def execute(self, context):
-        props = context.scene.addongen_render_output_snippets_props
-        rootPath = props.customRoot
-        fileName = "NoFileName" if os.path.basename(bpy.data.filepath).split(".")[0] == "" else os.path.basename(bpy.data.filepath).split(".")[0]
-        sceneName = bpy.context.scene.name
-        layerName = bpy.context.view_layer.name
-        cameraName = "NoCamera" if bpy.context.scene.camera == None else bpy.context.scene.camera.name
-        customFolder = props.customFolder
-        
-        renderpath = "C:\\"
-        renderpath = rootPath if props.rootPath else renderpath
-        renderpath = renderpath + fileName + "\\" if props.fileName else renderpath
-        renderpath = renderpath + sceneName + "\\" if props.sceneName else renderpath
-        renderpath = renderpath + layerName + "\\" if props.layerName else renderpath
-        renderpath = renderpath + cameraName + "\\" if props.cameraName else renderpath
-        renderpath = renderpath + customFolder + "\\" if props.customName else renderpath
-        
-        renderpath = renderpath + fileName + props.fseparator if props.ffileName else renderpath
-        renderpath = renderpath + sceneName + props.fseparator if props.fsceneName else renderpath
-        renderpath = renderpath + layerName + props.fseparator if props.flayerName else renderpath
-        renderpath = renderpath + cameraName + props.fseparator if props.fcameraName else renderpath
-        renderpath = renderpath + props.fcustomFileName + props.fseparator if props.fcustomName else renderpath
-        
-        if not props.fseparatorEnd:
-            renderpath = renderpath[:-len(props.fseparator)]
-            
-        
-        bpy.context.scene.render.filepath = renderpath
+        bpy.context.scene.render.filepath = GetPath(context)
         self.report({'INFO'}, "Path seted")
         return {'FINISHED'}
 
-    #def invoke(self, context, event):
-    #    wm.modal_handler_add(self)
-    #    return {'RUNNING_MODAL'}
-    #    return wm.invoke_porps_dialog(self)
-    #def modal(self, context, event):
-    #def draw(self, context):
 
 class RenderOutputSnippetsPanel(bpy.types.Panel):
     """Docstring of RenderOutputSnippetsPanel"""
     bl_idname = "RENDER_PT_render_output_snippets"
-    bl_label = "Render Output Snippets Panel"
-    
+    bl_label = "Render Output Snippets"
+
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_context = 'output'
 
-    #Panels in ImageEditor are using .poll() instead of bl_context.
-    #@classmethod
-    #def poll(cls, context):
-    #    return context.space_data.show_paint
-
     def draw(self, context):
-        props = context.scene.addongen_render_output_snippets_props
+        props = context.scene.render_output_snippets_props
         layout = self.layout
         layout.use_property_split = True
         layout.use_property_decorate = False  # No animation.
-        col = layout.column(align = True, heading ="Folder Path use")
-        col.prop(props, 'rootPath', text='Root Path')
+        col = layout.column(align=True, heading="Folder Path to use")
+        # col.prop(props, 'rootPath', text='Root Path')
         col.prop(props, 'fileName', text='File Name')
         col.prop(props, 'sceneName', text='Scene Name')
         col.prop(props, 'layerName', text='Layer Name')
-        col.prop(props, 'cameraName', text='Camera Name')
+        col.prop(props, 'cameraName', text='Active Camera Name')
         col.prop(props, 'customName', text='Custom Folder')
-        
-        col = layout.column(align = True, heading = "File Name use")
+
+        col = layout.column(align=True, heading="File Name to use")
         col.prop(props, 'ffileName', text='File Name')
         col.prop(props, 'fsceneName', text='Scene Name')
         col.prop(props, 'flayerName', text='Layer Name')
-        col.prop(props, 'fcameraName', text='Camera Name')
+        col.prop(props, 'fcameraName', text='Active Camera Name')
         col.prop(props, 'fcustomName', text='Custom Name')
         col.prop(props, 'fseparatorEnd', text='Use separator at End')
-        
-        
-        col = layout.column(align = True, heading = "Custom Paths")
-        col.prop(props, 'customRoot', text='Root path')
+
+        col = layout.column(align=True, heading="Custom Paths")
+        # col.prop(props, 'folderPath', text='Folder path')
+        col.prop(props, 'folderPath')
         col.prop(props, 'customFolder', text='Folder Name')
         col.prop(props, 'fcustomFileName', text='File Name')
-        col.prop(props, 'fseparator', text='Text Separator')
+        col.prop(props, 'fseparator', text='File name separator')
 
-        col = layout.column(align = True)
-        col.operator("addongen.render_output_snippets_operator", text = "Set Path")
+        col = layout.column(align=True)
+        col.operator("leo_tools.render_output_snippets_operator",
+                     text="Set Path")
 
-        
+        col = layout.column(align=True, heading="Preview path")
+        col.label(text="Path: " + GetPath(context))
+
 
 class RenderOutputSnippetsProps(bpy.types.PropertyGroup):
-    rootPath:     BoolProperty(name="", description="Use rootPath", default=True)
-    fileName:     BoolProperty(name="", description="Actual file name", default=False)
-    sceneName:     BoolProperty(name="", description="Current scene name", default=False)
-    layerName:     BoolProperty(name="", description="Current Layer name", default=False)
-    cameraName:     BoolProperty(name="", description="Active camera", default=False)
-    customName:     BoolProperty(name="", description="Use custom folder", default=False)
-    customFolder:   StringProperty(name="", description="folder name", default="MyFolder", maxlen=0)
-    customRoot:   StringProperty(name="", description="Custom Root path", default="//..\\render\\", maxlen=0)
-    
-    ffileName:     BoolProperty(name="", description="Actual file name", default=True)
-    fsceneName:     BoolProperty(name="", description="Current scene name", default=False)
-    flayerName:     BoolProperty(name="", description="Current Layer name", default=False)
-    fcameraName:     BoolProperty(name="", description="Active camera", default=False)
-    fcustomName:     BoolProperty(name="", description="Use custom file name", default=False)
-    fseparatorEnd:     BoolProperty(name="", description="Use separator at end of file name", default=False)
-    fcustomFileName:   StringProperty(name="", description="file name", default="MyFile", maxlen=0)
-    fseparator:   StringProperty(name="", description="file name Separator Simbol", default=" - ", maxlen=0)
-    
+    rootPath:     BoolProperty(
+        name="", description="Use rootPath", default=True)
+    fileName:     BoolProperty(
+        name="", description="Actual file name", default=True)
+    sceneName:     BoolProperty(
+        name="", description="Current scene name", default=False)
+    layerName:     BoolProperty(
+        name="", description="Current Layer name", default=False)
+    cameraName:     BoolProperty(
+        name="", description="Active camera", default=False)
+    customName:     BoolProperty(
+        name="", description="Use custom folder", default=False)
+    customFolder:   StringProperty(
+        name="", description="folder name", default="MyFolder", maxlen=0)
+    folderPath:   StringProperty(
+        name="Folder Path", description="Custom folder path", default="//..\\render\\", maxlen=1024, subtype="DIR_PATH")
+
+    ffileName:     BoolProperty(
+        name="", description="Actual file name", default=True)
+    fsceneName:     BoolProperty(
+        name="", description="Current scene name", default=False)
+    flayerName:     BoolProperty(
+        name="", description="Current Layer name", default=False)
+    fcameraName:     BoolProperty(
+        name="", description="Active camera", default=False)
+    fcustomName:     BoolProperty(
+        name="", description="Use custom file name", default=False)
+    fseparatorEnd:     BoolProperty(
+        name="", description="Use separator at end of file name", default=True)
+    fcustomFileName:   StringProperty(
+        name="", description="file name", default="MyRender", maxlen=0)
+    fseparator:   StringProperty(
+        name="", description="file name Separator Simbol", default="_", maxlen=0)
+
 
 def register():
     bpy.utils.register_class(RenderOutputSnippetsProps)
-    bpy.types.Scene.addongen_render_output_snippets_props = PointerProperty(type = RenderOutputSnippetsProps)
+    bpy.types.Scene.render_output_snippets_props = PointerProperty(
+        type=RenderOutputSnippetsProps)
 
     bpy.utils.register_class(RenderOutputSnippetsOperator)
     bpy.utils.register_class(RenderOutputSnippetsPanel)
 
+
 def unregister():
     bpy.utils.unregister_class(RenderOutputSnippetsProps)
-    #del bpy.types.Scene.addongen_render_output_snippets_props
 
     bpy.utils.unregister_class(RenderOutputSnippetsOperator)
     bpy.utils.unregister_class(RenderOutputSnippetsPanel)
+
 
 if __name__ == "__main__":
     register()
